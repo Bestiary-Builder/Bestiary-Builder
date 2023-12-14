8705a7762ff8ee5fc1519edea08cb96db8ac1df4
VeryGreatFrog <bvl46z@gmail.com>
2023-12-14 15:20:54 +0100

    minor css changes

--- frontend/src/constantComponents/NavHeader.vue
+++ frontend/src/constantComponents/NavHeader.vue
@@ -1,24 +1,27 @@
 <template>
 <input id="navbar-indicator" class="navbar-collapse" type="checkbox" checked>
 <nav class="navbar">
-
-
 	<div class="navbar-left">
+		<a class="navbar-brand" href="#">
+			B E S T I A R Y  B U I L D E R
+		</a>
 		<RouterLink v-for="route in $router.options.routes.filter((a: any) => a.navbar)" :to="route.path" class="nav-link">
 			<div class="header-item"> {{   route.name  }}</div>
 		</RouterLink>
 	</div>
 
 	<div class="navbar-center">
-		<a class="navbar-brand" href="#">
-			B E S T I A R Y  B U I L D ER
-		</a>
+		<!-- <RouterLink v-for="route in $router.options.routes.filter((a: any) => a.navbar)" :to="route.path" class="nav-link">
+			<div class="header-item"> {{   route.name  }}</div>
+		</RouterLink> -->
 	</div>
 
 	<div class="navbar-right">
-		<a class="nav-link" href="#">Github</a>
-		<a class="nav-link" href="#">Twitter</a>
-		<a class="nav-link" href="#">Facebook</a>
+		<a class="user" v-if="user" href="/user">
+			<span>@{{ user.username }}</span>
+			<img alt="avatar" :src="'https://cdn.discordapp.com/avatars/' + user._id + '/' + user.avatar + '.png'" />
+		</a>
+		<div v-else class="user"> Login </div>
 	</div>
 
 	<label class="navbar-toggler" for="navbar-indicator">
@@ -27,13 +30,48 @@
 </nav>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
 import {RouterLink, RouterView} from "vue-router";
+import UserBanner from "@/components/UserBanner.vue";
+import {user} from "@/main";
+import type {User} from "@/components/types";
+import {defineComponent} from "vue";
+import { handleApiResponse } from "@/main";
 export default defineComponent({
-	name: "NavHeader"
-})
+	name: "NavHeader",
+	components: {
+		UserBanner
+	},
+	data: () => ({key: 0} as {user: User | null; key: number}),
+	async mounted() {
+		this.user = (await fetch("/api/user").then(async (response: any) => {
+			if (response.status == 200) return await response.json();
+			else return null;
+		})) as User;
+		console.log(this.user);
+		this.$forceUpdate();
+	},
+	methods: {
+		LoginClick() {
+			window.location.href = "https://discord.com/oauth2/authorize?client_id=1183362236509601813&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Flogin&scope=identify+email";
+		}
+	},
+});
 </script>
 <style scoped lang="less">
+.user {
+	display: flex;
+	align-items: center;
+	padding: 1rem;
+	gap: .3rem;
+	& img {
+		height: 1.5rem;
+		border-radius: 50rem;
+		scale: 1.1
+	}
+}
+.user img {
+	border-radius: 50rem;
+}
 .navbar {
    position: relative;
 
@@ -41,7 +79,9 @@ export default defineComponent({
 
    display: flex;
    flex: 1;
-   justify-content: center;
+   justify-content: space-between;
+
+   box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
 }
 
 .navbar-left {
@@ -50,6 +90,7 @@ export default defineComponent({
 
 .navbar-right {
    display: inline-block;
+   display: inline-flex;
 }
 
 .navbar-center {
@@ -63,6 +104,7 @@ export default defineComponent({
 
    display: inline-block;
    padding: 1rem;
+
 }
 
 .navbar-brand {
@@ -74,7 +116,17 @@ export default defineComponent({
    background: orangered;
    
 }
-
+.nav-link {
+	min-width: 10vw;
+	text-align: center;
+	border-left: 1px solid orangered;
+		
+	text-transform: lowercase;
+	
+}
+.nav-link:last-of-type {
+	border-right: 1px solid orangered
+}
 .navbar .nav-link:hover {
    color: LightGray;
 }
