b0303f65b81aa8ad8419aace1f5a3fc8e1e2ac3d
Stevnbak <kasper@stevnbak.dk>
2023-12-19 21:00:34 +0000

    Update frontend-dist to output generated at c8af174

--- assets/HomeView-7557a11a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as _,c as o}from"./index-4551c236.js";const c={};function t(n,a){return _(),o("h1",null,"Home")}const r=e(c,[["render",t],["__scopeId","data-v-92e3a185"]]);export{r as default};
---
+++ assets/NotFound-8d400b49.js
@@ -0,0 +1,1 @@
+import{d as o,_ as e,o as n,c as t}from"./index-f02c2614.js";const c=o({mounted(){console.log("404")}});function r(s,a,d,p,_,f){return n(),t("h1",null,"Eror 404 - Page not found")}const u=e(c,[["render",r]]);export{u as default};
