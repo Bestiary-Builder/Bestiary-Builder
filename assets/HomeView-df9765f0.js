d6fa00b18788ced9699b3129b14c1eca2ad39560
Stevnbak <kasper@stevnbak.dk>
2023-12-19 20:19:48 +0000

    Update frontend-dist to output generated at 1dee3c1

---
+++ assets/HomeView-df9765f0.js
@@ -0,0 +1,1 @@
+import{_ as e,o as _,c as o}from"./index-31476f68.js";const c={};function t(n,a){return _(),o("h1",null,"Home")}const r=e(c,[["render",t],["__scopeId","data-v-92e3a185"]]);export{r as default};
