a1da2fbf09b6e55cb525ae61dcdbdf013b2c88ee
Stevnbak <kasper@stevnbak.dk>
2023-12-19 21:12:46 +0000

    Update frontend-dist to output generated at 94d35ac

---
+++ assets/TermsAndConditions-02575af8.js
@@ -0,0 +1,33 @@
+import{M as a}from"./index-3c6563bf.js";import{d as s,_ as i,o as n,c as t,f as r,p as l,g as c,a as o}from"./index-bbe52a9e.js";const d=`## General scope
+
+The following are the rules and guidelines for content created or otherwise added to Bestiary Builder.
+You do NOT need to publish your content publicly, but even if the content is private, it is subject to these rules & guidelines.
+Any content that violates these rules and guidelines will be removed, and the creator(s) may be subject to restrictions or other punishments.
+
+## Unacceptable content for Public Content
+
+### Naming and Shaming
+
+We don't want to see one sided accusations or any other form of naming and shaming as part of any content.
+
+### Personal Information
+
+Personal information about yourself or anyone else should also not be posted in any form of content.
+
+### Spamming and Trolling
+
+Neither spamming nor trolling is acceptable as part of any creation. This includes but isn’t limited to:
+creating content for the sole purpose of causing unrest, excessively communicating the same phrase, similar phrases,
+or pure gibberish, posting meme or troll text as part of the content.
+
+## Included content that will result in a ban
+
+-   Illegal PDF/ download of official content or the websites that create/use them.
+-   Keyloggers, hoaxes, and/or phishing websites.
+-   Content that is considered illegal by law or by Bestiary Builder.
+-   Religious opinions/debates from the real world (fantasy religious principles are generally okay, but may be removed).
+-   Hateful language about race, religion, country, political beliefs, and more.
+-   Sexually explicit content
+-   Referral links for contests, promotions, or anything else.
+-   Advertisements for other services
+`,u=a(),p=s({data(){return{content:u.render(d)}}});const m=e=>(l("data-v-253a4afa"),e=e(),c(),e),h={class:"content"},f=m(()=>o("h1",null,[o("span",null,"terms and conditions")],-1)),g=["innerHTML"];function y(e,b,_,v,w,I){return n(),t("div",h,[f,e.content?(n(),t("div",{key:0,innerHTML:e.content},null,8,g)):r("",!0)])}const B=i(p,[["render",y],["__scopeId","data-v-253a4afa"]]);export{B as default};
