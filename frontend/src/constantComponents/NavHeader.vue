730076ec65df2d2a0fb92dfa3f45a38555f01672
Stevnbak <kasper@stevnbak.dk>
2023-12-21 13:04:49 +0100

    Logo

--- frontend/src/constantComponents/NavHeader.vue
+++ frontend/src/constantComponents/NavHeader.vue
@@ -2,7 +2,10 @@
 	<input id="navbar-indicator" class="navbar-collapse" type="checkbox" checked />
 	<nav class="navbar">
 		<div class="navbar-left">
-			<RouterLink :to="$router.options.routes[0].path" class="navbar-brand"> BESTIARY BUILDER </RouterLink>
+			<RouterLink :to="$router.options.routes[0].path" class="navbar-brand">
+				<img src="../assets/logo.svg" />
+				<span>BESTIARY BUILDER</span>
+			</RouterLink>
 			<RouterLink v-for="route in $router.options.routes.filter((a: any) => a.navbar)" :to="route.path" class="nav-link">
 				<div class="header-item">{{ route.name }}</div>
 			</RouterLink>
@@ -82,7 +85,9 @@ export default defineComponent({
 }
 
 .navbar-left {
-	display: inline-block;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
 }
 
 .navbar-right {
@@ -110,6 +115,14 @@ export default defineComponent({
 	padding-right: 2rem;
 
 	background: orangered;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+}
+.navbar-brand img {
+	height: 2rem;
 }
 .nav-link {
 	min-width: 10vw;
