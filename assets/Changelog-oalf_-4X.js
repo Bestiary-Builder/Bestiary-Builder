4361f40c6b42d37e6a75cc3512fe0aadecae74e6
VeryGreatFrog <bvl46z@gmail.com>
2023-12-31 00:02:04 +0000

    Update frontend-dist to output generated at 71d4b8a

--- assets/Changelog-oalf_-4X.js
+++ assets/Changelog-oalf_-4X.js
@@ -1,4 +1,4 @@
-import{M as s}from"./index-5Jdbla-E.js";import{B as r}from"./Breadcrumbs-FL9n18Br.js";import{d as a,_ as d,r as i,o as t,c as n,a as c,b as m,i as h,F as p}from"./index-WoeFKfUp.js";const u=`## v1.0.0
+import{M as s}from"./index-5Jdbla-E.js";import{B as r}from"./Breadcrumbs-lt31y9MY.js";import{d as a,_ as d,r as i,o as t,c as n,a as c,b as m,i as h,F as p}from"./index-W9lxo0pW.js";const u=`## v1.0.0
 *January 2nd, 2024*
 - This is the first thing we added
 - This is the second thing we added
