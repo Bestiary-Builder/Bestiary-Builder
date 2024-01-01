4f1f57c7fb8c0f5bac9b004a500ede0914d6b025
VeryGreatFrog <bvl46z@gmail.com>
2024-01-01 22:13:35 +0000

    Update frontend-dist to output generated at 4af36bf

--- assets/PrivacyPolicy-JowJvWvV.js
+++ assets/PrivacyPolicy-JowJvWvV.js
@@ -1,4 +1,4 @@
-import{M as o}from"./index-5Jdbla-E.js";import{B as s}from"./Breadcrumbs-GQi51-sO.js";import{d as a,_ as r,r as l,o as n,c as t,a as c,b as d,i as u,F as h}from"./index-95-H25Lj.js";const y=`## General scope
+import{M as o}from"./index-5Jdbla-E.js";import{B as s}from"./Breadcrumbs-6XWf9OQn.js";import{d as a,_ as r,r as l,o as n,c as t,a as c,b as d,i as u,F as h}from"./index-mobFMn19.js";const y=`## General scope
 
 This policy covers personally identifiable information collected or
 stored by Bestiary Builder on its servers in relation to the service it
