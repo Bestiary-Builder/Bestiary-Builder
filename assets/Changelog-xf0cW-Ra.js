1fab9694c4bd429c476af5a769d74b14b6844cc2
VeryGreatFrog <bvl46z@gmail.com>
2024-01-03 23:36:27 +0000

    Update frontend-dist to output generated at 57c4a59

--- assets/Changelog-xf0cW-Ra.js
+++ assets/Changelog-xf0cW-Ra.js
@@ -1,4 +1,4 @@
-import{M as s}from"./index-5Jdbla-E.js";import{B as r}from"./Breadcrumbs-PImWGBWa.js";import{d as a,_ as d,r as i,o as t,c as n,a as c,b as m,i as h,F as p}from"./index-zcx6XJHO.js";const u=`## v1.0.0
+import{M as s}from"./index-5Jdbla-E.js";import{B as r}from"./Breadcrumbs-QDmMf9r_.js";import{d as a,_ as d,r as i,o as t,c as n,a as c,b as m,i as h,F as p}from"./index--pCCCWyf.js";const u=`## v1.0.0
 *January 2nd, 2024*
 - This is the first thing we added
 - This is the second thing we added
