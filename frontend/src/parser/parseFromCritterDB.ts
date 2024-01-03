0addad56556f58f397b1404c22837c173ddd23c5
VeryGreatFrog <bvl46z@gmail.com>
2024-01-03 17:03:53 +0100

    Update parseFromCritterDB.ts

--- frontend/src/parser/parseFromCritterDB.ts
+++ frontend/src/parser/parseFromCritterDB.ts
@@ -512,11 +512,13 @@ function innateSpellListConstructor(spellString: string): InnateSpellsEntity[] {
 						.trim()
 						.replace(/[.$*_]/g, "")
 						.replace(/\(.+\)/, "")
+						.trim()
 			) ||
 			sp
 				.trim()
 				.replace(/[.$*_]/g, "")
-				.replace(/\(.+\)/, "");
+				.replace(/\(.+\)/, "")
+				.trim()
 		output.push({
 			spell: spell,
 			comment: comment
