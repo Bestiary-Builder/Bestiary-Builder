2e0b82ed5f10ca284a72967c288b151a5feb4498
VeryGreatFrog <bvl46z@gmail.com>
2024-01-05 20:07:03 +0000

    Update frontend-dist to output generated at e7ec822

--- assets/Changelog-tLlI24xD.js
+++ assets/Changelog-tLlI24xD.js
@@ -1,4 +1,4 @@
-import{M as s}from"./index-5Jdbla-E.js";import{B as r}from"./Breadcrumbs-v_AhjImO.js";import{d as a,_ as d,r as i,o as t,c as n,a as c,b as m,i as h,F as p}from"./index-UkN1H5bj.js";const u=`## v1.0.0
+import{M as s}from"./index-5Jdbla-E.js";import{B as r}from"./Breadcrumbs-FlUpfuIt.js";import{d as a,_ as d,r as i,o as t,c as n,a as c,b as m,i as h,F as p}from"./index--Tf8YunR.js";const u=`## v1.0.0
 *January 2nd, 2024*
 - This is the first thing we added
 - This is the second thing we added
