bd5c505f21abcc92c687f5f490733daf1877154a
Stevnbak <kasper@stevnbak.dk>
2024-01-12 11:13:06 +0000

    Update frontend-dist to output generated at 1522cad

---
+++ assets/Changelog-A41oP-5X.js
@@ -0,0 +1,17 @@
+import{M as r}from"./index-5Jdbla-E.js";import{B as s}from"./Breadcrumbs-StEeb8Pe.js";import{d as a,_ as c,r as i,o as n,c as t,a as m,b as d,i as l,F as p}from"./index-psA_ScK5.js";const _=`## v1.0.1 
+
+_January 12th, 2024_
+- Resolves some CritterDB importing as an empty Bestiary
+- Developer environment fix
+- Fix scrolling sometimes not working on mobile
+
+## v1.0.0
+
+_January 10th, 2024_
+Release
+
+## v0.0.0
+
+_December 10th, 2023_
+Development started
+`,u=r(),f=a({data(){return{content:u.render(_)}},components:{Breadcrumbs:s}}),v={class:"content markdown less-wide"},h=["innerHTML"];function B(e,g,C,b,k,y){const o=i("Breadcrumbs");return n(),t(p,null,[m(o,{routes:[{path:"",text:"Changelog",isCurrent:!0}],isLessWide:!0}),d("div",v,[e.content?(n(),t("div",{key:0,innerHTML:e.content},null,8,h)):l("",!0)])],64)}const F=c(f,[["render",B]]);export{F as default};
--- assets/HomeView-k57xOnwq.js
+++ assets/HomeView-k57xOnwq.js
@@ -1,4 +1,4 @@
-import{M as o}from"./index-5Jdbla-E.js";import{B as r}from"./Breadcrumbs-rq8K6Ogw.js";import{d as i,_ as s,r as c,o as e,c as n,a as d,b as p,i as l,F as h}from"./index-9W9gHBt8.js";const u=`<img src="/logo-text.svg" width="75%" style="margin: auto">
+import{M as o}from"./index-5Jdbla-E.js";import{B as r}from"./Breadcrumbs-StEeb8Pe.js";import{d as i,_ as s,r as c,o as e,c as n,a as d,b as p,i as l,F as h}from"./index-psA_ScK5.js";const u=`<img src="/logo-text.svg" width="75%" style="margin: auto">
 
 ---
 
