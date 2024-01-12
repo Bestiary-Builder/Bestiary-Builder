da8ab0b43713e2f37d7061ccc0555ab91286fe8c
VeryGreatFrog <bvl46z@gmail.com>
2024-01-12 10:56:08 +0000

    Update frontend-dist to output generated at aa19e84

---
+++ assets/Changelog-fsvwodby.js
@@ -0,0 +1,16 @@
+import{M as r}from"./index-5Jdbla-E.js";import{B as s}from"./Breadcrumbs-rq8K6Ogw.js";import{d as a,_ as c,r as m,o as n,c as t,a as d,b as i,i as p,F as _}from"./index-9W9gHBt8.js";const l=`## v1.0.1 
+
+_January 12th, 2024_
+- Resolves some CritterDB importing as an empty Bestiary
+- Developer environment fix
+
+## v1.0.0
+
+_January 10th, 2024_
+Release
+
+## v0.0.0
+
+_December 10th, 2023_
+Development started
+`,u=r(),f=a({data(){return{content:u.render(l)}},components:{Breadcrumbs:s}}),v={class:"content markdown less-wide"},h=["innerHTML"];function B(e,C,g,b,k,y){const o=m("Breadcrumbs");return n(),t(_,null,[d(o,{routes:[{path:"",text:"Changelog",isCurrent:!0}],isLessWide:!0}),i("div",v,[e.content?(n(),t("div",{key:0,innerHTML:e.content},null,8,h)):p("",!0)])],64)}const w=c(f,[["render",B]]);export{w as default};
--- assets/PrivacyPolicy-GHKUkOiO.js
+++ assets/PrivacyPolicy-GHKUkOiO.js
@@ -1,4 +1,4 @@
-import{M as o}from"./index-5Jdbla-E.js";import{B as s}from"./Breadcrumbs-JjDYZcFB.js";import{d as a,_ as r,r as l,o as n,c as t,a as c,b as d,i as u,F as h}from"./index-bwbZjrL1.js";const y=`## General scope
+import{M as o}from"./index-5Jdbla-E.js";import{B as s}from"./Breadcrumbs-rq8K6Ogw.js";import{d as a,_ as r,r as l,o as n,c as t,a as c,b as d,i as u,F as h}from"./index-9W9gHBt8.js";const y=`## General scope
 
 This policy covers personally identifiable information collected or
 stored by Bestiary Builder on its servers in relation to the service it
