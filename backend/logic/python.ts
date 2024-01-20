960a3eba12e91d3120d919bb4b06cb06b38aac11
Stevnbak <kasper@stevnbak.dk>
2024-01-21 00:02:02 +0100

    Automation validation api end point

--- backend/logic/python.ts
+++ backend/logic/python.ts
@@ -2,12 +2,13 @@ import {spawn} from "child_process";
 import {log} from "../logger";
 import {app} from "../server";
 import {time, timeEnd} from "console";
+import fetch from "node-fetch";
 
 //Install pip modules
-spawn("python", ["scripts/installPipModules.py"]);
+spawn("python3", ["scripts/installPipModules.py"]);
 //Script
 async function runPythonScript(args: string) {
-	const childProcess = spawn("python", ["scripts/python.py", args]);
+	const childProcess = spawn("python3", ["scripts/python.py", args]);
 	childProcess.stdout.setEncoding("utf-8");
 	childProcess.stderr.setEncoding("utf-8");
 	let output: string | null;
@@ -25,22 +26,29 @@ async function runPythonScript(args: string) {
 		});
 	});
 }
-//Run
-runTest(1);
-async function runTest(amount: number) {
-	time("python");
-	let scriptsDone = 1;
-	for (let i = 0; i < amount; i++) {
-		runPythonScript("" + i)
-			.then((result) => {
-				log.info("Succes: " + result);
-				scriptsDone++;
-				if (scriptsDone == amount) timeEnd("python");
-			})
-			.catch((error) => {
-				log.error(error);
-				scriptsDone++;
-				if (scriptsDone == amount) timeEnd("python");
-			});
-	}
-}
+
+app.post("/api/validate/automation", async (req, res) => {
+	let automation = req.body.data;
+	let result = await runPythonScript(JSON.stringify(automation));
+	console.log("Result", result);
+	if (result == "") return res.status(200).json({valid: true, error: null});
+	else return res.status(400).json({valid: false, error: result});
+});
+
+//Temporary test fetch
+fetch("http://localhost:5000/api/validate/automation", {
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json"
+	},
+	body: JSON.stringify({
+		data: [
+			{type: "target", target: "each", effects: [{type: "attack", hit: [{type: "damage", damage: "1d6 + 2 [slashing]", overheal: false}], miss: [], attackBonus: "4"}]},
+			{type: "text", text: "*Melee Weapon Attack:* +4 to hit, reach 5 ft., one target. *Hit:* 5 (1d6 + 2) slashing damage.", title: "Effect"}
+		]
+	})
+})
+	.then((response) => response.json())
+	.then((data) => {
+		console.log("data", data);
+	});
--- backend/scripts/python.py
+++ backend/scripts/python.py
@@ -1,10 +1,11 @@
 import sys
+import json
 from automation_common import validation
 from pydantic import ValidationError
 
 return_value = ""
 try:
-    validation.validate(sys.argv[1])
+    validation.validate(json.loads(sys.argv[1]))
 except ValidationError as e:
     return_value = e
 
