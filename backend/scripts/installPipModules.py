6dab53ec72edfc0c6e900e4d886c024d8c463789
VeryGreatFrog <bvl46z@gmail.com>
2024-01-21 00:06:26 +0100

    implement cool error handling (thanks zhu)

--- backend/scripts/installPipModules.py
+++ backend/scripts/installPipModules.py
@@ -4,3 +4,5 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 #Modules:
 install('git+https://github.com/avrae/automation-common.git')
+install('pydantic')
+install('typing')
