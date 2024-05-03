eb103a13a180f0312f3559fee7f28f38b7e21b00
Stevnbak <kasper@stevnbak.dk>
2024-05-03 16:42:45 +0200

    camelCase fixes #38

--- backend/tests/critterdb.test.ts
+++ backend/tests/critterdb.test.ts
@@ -1,8 +1,8 @@
-import { expect, it } from 'vitest';
-import { get_published_bestiary_creatures } from "../logic/critterdb";
+import {expect, it} from "vitest";
+import {getPublishedBestiaryCreatures} from "../logic/critterdb";
 
 it("CritterDB export to be unchanged", async () => {
-    // Test https://critterdb.com/#/publishedbestiary/view/5acb0aa187653a455731b890 to be identical as before.
-    const result = await get_published_bestiary_creatures("5acb0aa187653a455731b890", "https://critterdb.com:443/api/publishedbestiaries")
-    expect(result).toMatchSnapshot();
-})
+	// Test https://critterdb.com/#/publishedbestiary/view/5acb0aa187653a455731b890 to be identical as before.
+	const result = await getPublishedBestiaryCreatures("5acb0aa187653a455731b890", "https://critterdb.com:443/api/publishedbestiaries");
+	expect(result).toMatchSnapshot();
+});
