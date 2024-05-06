57b5df5dd39c0c952d0673805d8b067e2ccd0309
VeryGreatFrog <bvl46z@gmail.com>
2024-05-06 14:47:46 +0200

    fix conflict

--- shared/index.ts
+++ shared/index.ts
@@ -1,17 +1,3 @@
 export * from "./src/types";
 export * from "./src/statblock-helpers"
-import types from "./src/types-ti";
-export const typeInterface = types;
-
-import type {IErrorDetail} from "ts-interface-checker";
-
-export function interfaceValidation(validation: IErrorDetail[], nestlevel = 0 as number) {
-	let message = "";
-	for (let err of validation) {
-		message += `<span style="margin-left:${nestlevel}rem"><span style="font-family: monospace; font-size: 1rem;">${err.path.replace("value.", "")}</span> ${err.message}${err.nested ? ":" : "."}</span>\n`;
-		if (err.nested) {
-			message += interfaceValidation(err.nested, nestlevel + 1) ?? "";
-		}
-	}
-	return message.replaceAll("is not a", "is not of type");
-}
+export * as routes from "./src/routes"
