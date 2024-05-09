e96fab1973ba609abc147e96735f0a3ddb897fae
VeryGreatFrog <bvl46z@gmail.com>
2024-05-09 20:17:14 +0200

    move errorHandler

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -47,25 +47,11 @@ if (import.meta.env.MODE == "production") {
 	import("./utils/gtag.js" as any);
 }
 
+// error handling
+if (import.meta.env.MODE == "production") {
+	import("./utils/app/error")
+}
+
 //Finally, mount our app.
 app.mount("body");
 
-//Send errors to discord
-app.config.errorHandler = (err: any, instance, info) => {
-	console.error(err);
-	if (import.meta.env.PROD)
-		fetch(import.meta.env.VITE_ERROR_WEBHOOK, {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				content: `Frontend error: \n${err.stack ?? err}`,
-				username: "Bestiary Builder",
-				avatar_url: "https://bestiarybuilder.com/logo.png"
-			})
-		}).catch(() => {
-			console.error("Failed to send error message.");
-		});
-};
