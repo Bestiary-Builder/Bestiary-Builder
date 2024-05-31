1defb62068b3c33a5bd7566caba2280eae010409
VeryGreatFrog <bvl46z@gmail.com>
2024-05-31 09:27:42 +0000

    Update output to output generated at 5d5e26b

--- frontend/assets/changelog-DupL7Quv.js
+++ frontend/assets/changelog-DupL7Quv.js
@@ -5,6 +5,7 @@ _May 31st, 2024_
   - This means you no longer need to open a menu and paste the json directly. Simply press export on one page, and press paste on the other.
 - Fixes an issue where Animal Handling and Sleight of Hand skills would not show on the statblock since v2.0.0.
 
+---
 ## v2.2.0
 _May 29th, 2024_
 
