efcaa5a2dcbfdda5945eaaf930584f1cac0a6e48
VeryGreatFrog <bvl46z@gmail.com>
2025-01-11 22:22:28 +0000

    Update stage-output to output generated at 0cf1422

--- frontend/assets/error-Pqs1eKJt.js
+++
@@ -1,2 +0,0 @@
-import{a as o}from"./index-BmrAt_SM.js";o.config.errorHandler=(e,r,t)=>{fetch("https://discord.com/api/webhooks/1254166788384493721/dCpqJ4M9S-ofjGEe8bgDHYSmWdFsK22B7aMy3FOmzQeH6Wgm6gs6U-GKBjkfSmPX9Z1i",{method:"POST",headers:{Accept:"application/json","Content-Type":"application/json"},body:JSON.stringify({content:`Frontend error: 
-${e.stack??e}`,username:"Bestiary Builder",avatar_url:"https://bestiarybuilder.com/logo.png"})}).catch(()=>{console.error("Failed to send error message.")})};
