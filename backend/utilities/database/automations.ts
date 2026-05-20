2c8ab45b369f9a46c2ca57fc171b74399286ed3a
Stevnbak <kasper@stevnbak.dk>
2026-05-20 22:56:11 +0200

    Fix automation deletion

--- backend/utilities/database/automations.ts
+++ backend/utilities/database/automations.ts
@@ -43,7 +43,7 @@ export async function deleteAutomation(id: Id) {
 			const automation = await getAutomation(id);
 			if (!automation)
 				return false;
-			await getPrismaClient().bestiary.delete({ where: { id } });
+			await getPrismaClient().automation.delete({ where: { id } });
 			return true;
 		});
 	}
