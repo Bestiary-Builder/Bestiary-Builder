6f2ac19e006c5e3d6ffe0576d77db081d11dedf6
Kasper Stevnbak <kasper@stevnbak.dk>
2026-07-27 15:21:20 +0200

    Enable analytics in build

--- frontend/src/utils/app/analytics.ts
+++ frontend/src/utils/app/analytics.ts
@@ -1,4 +1,4 @@
-if (import.meta.env.MODE === "production" || true) {
+if (import.meta.env.MODE === "production") {
 	const script = document.createElement("script");
 	script.defer = true;
 	script.src = import.meta.env.VITE_UMAMI_URL;
